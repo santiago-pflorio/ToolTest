39514c66cef29bbbbe0546099da0ba72acbf9051
Santiago P.Florio <santiago.perez@southworks.net>
2014-04-22 17:30:41 -0300

    first commit

---
+++ TestApplication/ConsoleAppFixture/ConsoleApp.Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace ConsoleAppFixture
+{
+    [TestClass]
+    public class ConsoleAppTests
+    {
+        [TestMethod]
+        public void Debit_WhenAmountIsGreaterThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert
+                StringAssert.Contains(e.Message, BankAccount. DebitAmountExceedsBalanceMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+    }
+}
---
+++ TestApplication/TestApplication/BankAccountNS.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountNS
+{
+        /// <summary> 
+        /// Bank Account demo class. 
+        /// </summary> 
+        public class BankAccount
+        {
+            private string m_customerName;
+
+            private double m_balance;
+
+            private bool m_frozen = false;
+
+            public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+
+            public const string DebitAmountLessThanZeroMessage = "Debit amount less than zero";
+
+            public BankAccount()
+            {
+            }
+
+            public BankAccount(string customerName, double balance)
+            {
+                m_customerName = customerName;
+                m_balance = balance;
+            }
+
+            public string CustomerName
+            {
+                get { return m_customerName; }
+            }
+
+            public double Balance
+            {
+                get { return m_balance; }
+            }
+
+            public void Debit(double amount)
+            {
+                if (m_frozen)
+                {
+                    throw new Exception("Account frozen");
+                }
+
+                if (amount > m_balance)
+                {
+                    throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+                }
+
+                if (amount < 0)
+                {
+                    throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+                }
+
+                m_balance += amount;
+            }
+
+            public void Credit(double amount)
+            {
+                if (m_frozen)
+                {
+                    throw new Exception("Account frozen");
+                }
+
+                if (amount < 0)
+                {
+                    throw new ArgumentOutOfRangeException("amount");
+                }
+
+                m_balance += amount;
+            }
+
+            private void FreezeAccount()
+            {
+                m_frozen = true;
+            }
+
+            private void UnfreezeAccount()
+            {
+                m_frozen = false;
+            }
+        }
+    }
